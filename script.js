420b4a5337fcf32900bde0ddb94e5741fccf6400
deceive the world with a lies <98891424+IceHiroHentai@users.noreply.github.com>
2024-03-29 18:25:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let num = 15;
+
+if (isNaN(num)) {
+    console.log("Введите только числа");
+} else {
+  
+    if (num % 2 === 0) {
+        console.log("Число четное");
+    } else {
+        console.log("Число нечетное");
+    }
+}
+
